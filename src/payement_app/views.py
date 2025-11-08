a306dcfbd6928f4420e31e9f342fe6f6dd489e87
abd-nasser <nasbasas@gmail.com>
2025-11-08 16:39:25 +0100

    initiation de strip

--- src/payement_app/views.py
+++ src/payement_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Commande
+from .models import Paiement
+from order_app.models import Commande
+from django.conf import settings
+import stripe
+
+
 
 @login_required #views accecibles seulement quand user est connecté
 def choix_paiement(request, commande_id):
@@ -27,3 +32,54 @@ def choix_paiement(request, commande_id):
     
     
 @login_required
+def initier_sripe(request, commande_id):
+    """
+    LANCE LE PAIEMENT STRIPE (CARTES)
+    """
+    #"Trouvé la commande à payé"
+    commande = get_object_or_404(Commande, id=commande_id, panier__client=request.user)
+    
+    # convertit le prix en centimes
+    montant = int(commande.panier.total_panier()*100)
+    #Stripe veut 1000 pour 10,00(comme les centimes)
+    
+    try:
+        #Dit à stripe de préparer le paiement"
+        intent = stripe.PaymentIntent.create(
+            amount=montant,
+            currency='xof',
+            metadata={
+                'commande_id':commande.id,
+                'client_id':request.user.id
+            }
+        )
+         # 👆 Comme dire: "Hé Stripe, prépare-toi à recevoir un paiement!"
+         
+         # 🎯en meme temps on initialise les champs de model paiements dans db
+        paiement = Paiement.objects.create(
+            commande=commande,
+            methode="stripe",
+            montant=commande.panier.total_panier(),
+            statu="reussi",
+            stripe_payement_intent_id=intent.id,
+         )
+        
+        # initialise le champs paiement dans le model Commande 
+        commande.paiements=paiement
+        commande.save()
+        
+        #préparer l'ecran de paiement
+        ctx = {
+            "commande":commande,
+            "client_secret":intent.client_secret, #clé secrète pour Stripe
+            #"stripe_public_key":settings.STRIPE_PUBLIC_KEY #clé publique
+        }
+        # 🎯 Ligne 7: "Montre l'écran où on tape la carte"
+        return render(request, 'payement_templates/paiement_stripe.html', ctx)
+    
+    except stripe.StripeError as e:
+         # 🎯 Ligne 8: "Si erreur, affiche un message"
+         messages.error(request,f'Erreur Stripe:{str(e)}')
+         return redirect('payement_app:choix_paiement', commande_id=commande.id)
+      # 👆 Comme dire: "Le terminal ne marche pas, retourne au menu"
+    
